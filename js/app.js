32d68cf44e37b51f3f110851eebe7a78de615c32
Yony Muñeton <yonyton@hotmail.com>
2016-11-04 16:45:34 -0500

    New level field that show the level of thecat based on the number of clicks

--- js/app.js
+++ js/app.js
@@ -5,12 +5,25 @@ var ViewModel = function () {
 	this.name = ko.observable("tony");
 	this.clickCount = ko.observable(0);
 	this.imgSrc = ko.observable("https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426")
+	this.level=ko.observable("");
 
 	// This increment the clickCount when is the img is clicked
 	this.incrementClick = function () {
 		this.clickCount(this.clickCount() +1);
 	}
 
+	// Using a computed variable to process the cat level
+	this.catLevel = ko.computed(function(){
+		if (this.clickCount() > 20) {
+			return this.level ("Teenager");
+		}
+		else if (this.clickCount() > 10) {
+			return this.level ("Infant");
+		}
+		else {
+			return this.level("NewBorn");
+		}
+	},this)
 }
 
 ko.applyBindings(new ViewModel());
