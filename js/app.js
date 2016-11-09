5312b492e80f8a8f6aa799fd4c51c93c8226e9a8
Yony Muñeton <yonyton@hotmail.com>
2016-11-08 21:11:09 -0500

    New NickName List added

--- js/app.js
+++ js/app.js
@@ -7,6 +7,18 @@ var ViewModel = function () {
 	this.imgSrc = ko.observable("https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426")
 	this.level=ko.observable("");
 
+	this.nickNames = ko.observableArray (
+		[
+			{nickName: 'pilo'},
+			{nickName: 'coty'},
+			{nickName: 'jango'}
+		]
+		);
+	
+	this.removeNickName = function () {
+		this.nickNames.remove(this);
+	}
+
 	// This increment the clickCount when is the img is clicked
 	this.incrementClick = function () {
 		this.clickCount(this.clickCount() +1);
